539f2a82e129181b2685f356d2d1e305bf315c14
oooi0001 <oooi0001@student.monash.edu>
2025-10-12 09:41:56 +0800

    Finished base charts

---
+++ idioms/choropleth_map/unemployment_map.js
@@ -0,0 +1,3 @@
+var vg = "idioms/choropleth_map/unemployment_map.vg.json";
+
+vegaEmbed("#unemployment_map", vg).then(function(result){}).catch(console.error);
---
+++ idioms/line_chart/national_unemployment_trend.js
@@ -0,0 +1,3 @@
+var vg = "idioms/line_chart/national_unemployment_trend.vg.json";
+
+vegaEmbed("#national_unemployment_trend", vg).then(function(result){}).catch(console.error);
